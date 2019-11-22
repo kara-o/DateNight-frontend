18a55d61b0b61f183af156b365eb5c40e43c6775
kara-o <44376276+kara-o@users.noreply.github.com>
2019-11-22 12:30:49 -0800

    removed currentUser propr from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Navbar = props => {
   return (
     <div>
-      <h3>{props.currentUser.username}</h3>
       <Link
         to='/login'
         onClick={() => {
