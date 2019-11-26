9d8cd5e6f10590cda0110cadca93443bd4025497
Kara Odle <karajo.odle@gmail.coml>
2019-11-25 21:59:49 -0800

    sidebar - avatar, username

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
 
-const Sidebar = () => {
-  return <div className='sidebar'>This is the sidebar.</div>;
+const Sidebar = ({ currentUserData }) => {
+  const currentUser = currentUserData.user;
+  console.log(currentUser);
+
+  const renderInitials = () => {
+    return currentUser
+      ? `${currentUser.first_name.slice(0, 1)}` +
+          `${currentUser.last_name.slice(0, 1)}`
+      : null;
+  };
+
+  return (
+    <div className='sidebar'>
+      <Avatar id='avatar'>{renderInitials()}</Avatar>
+      {currentUser ? (
+        <ul id='sidebar-list'>
+          <li>
+            <strong>{currentUser.username}</strong>
+          </li>
+          <li>Member Since: {currentUser.join_date}</li>
+        </ul>
+      ) : null}
+    </div>
+  );
 };
 
 export default Sidebar;
