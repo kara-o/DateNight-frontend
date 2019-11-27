de343c1964f4c192a6a2dc9144ee19acc9deaf91
kara-o <44376276+kara-o@users.noreply.github.com>
2019-11-27 12:09:32 -0800

    admin request table, show page

--- src/user/components/Main.js
+++ src/user/components/Main.js
@@ -6,16 +6,16 @@ import itinerary from '../images/itinerary1.png';
 const Main = ({ currentUser, token }) => {
   const [requests, setRequests] = useState(null);
 
-  const convertTime = string => {
-    let end = '';
-    const arr = string.split(':');
-    let first = parseInt(arr[0], 10);
-    if (first > 12) {
-      end = 'PM';
-      first -= 12;
-    }
-    return `${first}:${arr[1]} ${end}`;
-  };
+  // const convertTime = string => {
+  //   let end = '';
+  //   const arr = string.split(':');
+  //   let first = parseInt(arr[0], 10);
+  //   if (first > 12) {
+  //     end = 'PM';
+  //     first -= 12;
+  //   }
+  //   return `${first}:${arr[1]} ${end}`;
+  // };
 
   const handleClick = e => {
     console.log('itinerary!');
@@ -29,21 +29,14 @@ const Main = ({ currentUser, token }) => {
 
   const renderRequests = () => {
     if (requests) {
+      console.log(requests);
       return requests.map(r => {
         return (
-          <div key={r.id} id='request-row'>
+          <li key={r.id} id='request-row'>
             <Link to={`/requests/${r.id}`} id='request-row-link'>
               <ul className='link-list'>
-                <li>{new Date(r.date).toDateString()}</li>
-                <li>
-                  {convertTime(
-                    new Date(r.start_time).toTimeString().split(':00 ')[0]
-                  ) +
-                    ' - ' +
-                    convertTime(
-                      new Date(r.end_time).toTimeString().split(':00 ')[0]
-                    )}
-                </li>
+                <li>{r.date}</li>
+                <li>{r.start + ' - ' + r.end}</li>
                 <li>{r.size}</li>
                 <li>
                   <i>{r.status}</i>
@@ -59,7 +52,7 @@ const Main = ({ currentUser, token }) => {
                 </li>
               </ul>
             </Link>
-          </div>
+          </li>
         );
       });
     }
@@ -70,17 +63,15 @@ const Main = ({ currentUser, token }) => {
       <Link className='new-request-link' to='/request'>
         Make a New Request!
       </Link>
-      <div className='request-list'>
-        <div id='request-headers'>
-          <ul className='link-list'>
-            <li>Date</li>
-            <li>Window</li>
-            <li>Party Size</li>
-            <li>Status</li>
-            <li>Itinerary</li>
-          </ul>
-        </div>
-        {renderRequests()}
+      <div id='request-list-div'>
+        <ul className='header-list'>
+          <li>Date</li>
+          <li>Window</li>
+          <li>Party Size</li>
+          <li>Status</li>
+          <li>Itinerary</li>
+        </ul>
+        <ul className='request-list'>{renderRequests()}</ul>
       </div>
     </div>
   );
