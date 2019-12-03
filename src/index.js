129590eba8066cf7e1bae7352cbdaba2b9fa394e
kara-o <44376276+kara-o@users.noreply.github.com>
2019-12-03 13:26:31 -0800

    refactored css file structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './user/components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './main.css';
+import './navigation.css';
+import './form.css';
+import './sidebar.css';
 
 ReactDOM.render(
   <Router>
