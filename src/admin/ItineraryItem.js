00a6673f7476237a084c40ff5937dad144343ba5
kara-o <44376276+kara-o@users.noreply.github.com>
2019-12-05 10:03:11 -0800

    fixed itin and req layout

--- src/admin/ItineraryItem.js
+++ src/admin/ItineraryItem.js
@@ -7,23 +7,30 @@ const ItineraryItem = props => {
   const { item, admin } = props;
   return (
     <>
-      {admin || item.arrival_time < new Date() ? (
-        <Paper elevation={10} className='paper itin-item-display'>
-          <div className='details'>
-            <h3>Details</h3>
-            <div className='details-p'>
-              <p>{moment(item.arrival_time).format('h:mm a')}</p>
-              <p>{item.place}</p>
-              <p>{item.address}</p>
-              <p>{item.blurb}</p>
+      <Paper elevation={10} className='paper itin-item-display'>
+        <div className='details'>
+          <h3>Details</h3>
+          <div className='details-p'>
+            <p>{moment(item.arrival_time).format('h:mm a')}</p>
+            <p>{item.place}</p>
+            <p>{item.address}</p>
+            <p>{item.blurb}</p>
+            {admin && item.make_res_link ? (
               <p>
-                <a href={item.map_url}>Google Map</a>
+                <a href={item.make_res_link} target='_blank'>
+                  Make Reservation
+                </a>
               </p>
-            </div>
+            ) : null}
+            <p>
+              <a href={item.map_url} target='_blank'>
+                Google Map
+              </a>
+            </p>
           </div>
-          <Map url={item.map_iframe_url} />
-        </Paper>
-      ) : null}
+        </div>
+        <Map url={item.map_iframe_url} />
+      </Paper>
     </>
   );
 };
