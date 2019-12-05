ed6e562721f829a87f2c749b907f579a04eb0f9d
Kara Odle <karajo.odle@gmail.coml>
2019-12-04 16:58:42 -0800

    layout changes

--- src/admin/ItineraryItem.js
+++ src/admin/ItineraryItem.js
@@ -9,14 +9,18 @@ const ItineraryItem = props => {
     <>
       {admin || item.arrival_time < new Date() ? (
         <Paper elevation={10} className='paper itin-item-display'>
-          <h3>Details</h3>
-          <p>{moment(item.arrival_time).format('h:mm a')}</p>
-          <p>{item.place}</p>
-          <p>{item.address}</p>
-          <p>{item.blurb}</p>
-          <p>
-            <a href={item.map_url}>Google Map</a>
-          </p>
+          <div className='details'>
+            <h3>Details</h3>
+            <div className='details-p'>
+              <p>{moment(item.arrival_time).format('h:mm a')}</p>
+              <p>{item.place}</p>
+              <p>{item.address}</p>
+              <p>{item.blurb}</p>
+              <p>
+                <a href={item.map_url}>Google Map</a>
+              </p>
+            </div>
+          </div>
           <Map url={item.map_iframe_url} />
         </Paper>
       ) : null}
