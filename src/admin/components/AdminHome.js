04b7a83ae895f73db34e5f640cc071cc95f9d678
kara-o <44376276+kara-o@users.noreply.github.com>
2020-05-01 14:17:18 -0700

    admin home removed filter, add menu bar

--- src/admin/components/AdminHome.js
+++ src/admin/components/AdminHome.js
@@ -1,45 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { fetchRequests } from '../services/api-admin';
-import { ListContainer, ListItem, Filter, Stars } from '../../elements';
-import * as moment from 'moment';
-import { createUseStyles } from 'react-jss';
+import React, { useState, useEffect } from "react";
+import { fetchRequests } from "../services/api-admin";
+import { ListContainer, ListItem, Filter, Stars, Button } from "../../elements";
+import * as moment from "moment";
+import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
-  requests: {
-    gridColumn: '1/3',
-    gridRow: 'auto',
+  mainContainer: {
+    gridColumn: "1/3",
+    gridRow: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  buttonsDiv: {
+    marginTop: "20px",
   },
   filter: {
-    padding: '20px 0px 20px 0px',
+    padding: "20px 0px 20px 0px",
   },
   redText: {
-    color: 'red',
+    color: "red",
+  },
+  italicFont: {
+    fontStyle: "italic",
   },
 });
 
 const AdminHome = (props) => {
   const { userData } = props;
   const [allRequests, setAllRequests] = useState([]);
-  const [filter, setFilter] = useState('Unfulfilled');
+  const [cancelledRequests, setCancelledRequests] = useState({
+    cancelled: [],
+    show: false,
+  });
+  const [newlyReviewed, setNewlyReviewed] = useState({
+    newlyReviewed: [],
+    show: false,
+  });
+  const [filter, setFilter] = useState("Unfulfilled");
   const classes = useStyles();
 
   useEffect(() => {
     if (userData) {
-      fetchRequests(userData).then((json) => setAllRequests(json));
+      fetchRequests(userData).then((json) => {
+        setAllRequests(json);
+        setCancelledRequests({
+          ...cancelledRequests,
+          cancelled: json.filter((r) => r.cancelled && r.fulfilled),
+        });
+        setNewlyReviewed({
+          ...newlyReviewed,
+          newlyReviewed: json.filter(
+            (r) => r.review && !r.review.admin_reviewed
+          ),
+        });
+      });
     }
   }, []);
 
   const renderRequests = () => {
     let requests;
-    if (filter === 'Unfulfilled') {
+    if (cancelledRequests.show) {
+      requests = cancelledRequests.cancelled;
+    } else if (newlyReviewed.show) {
+      requests = newlyReviewed.newlyReviewed;
+    } else if (filter === "Unfulfilled") {
       requests = allRequests.filter((r) => !r.fulfilled);
-    } else if (filter === 'New Reviews') {
-      requests = allRequests.filter(
-        (r) => r.review && !r.review.admin_reviewed
-      );
-    } else if (filter === 'Cancelled') {
-      requests = allRequests.filter((r) => r.cancelled);
-    } else if (filter === 'Completed') {
+    } else if (filter === "Completed") {
       requests = allRequests.filter(
         (r) =>
           r.fulfilled && !r.cancelled && new Date(r.start_time) < new Date()
@@ -52,17 +79,17 @@ const AdminHome = (props) => {
       let status;
       if (r.fulfilled) {
         if (new Date(r.start_time) < new Date() && !r.cancelled) {
-          status = 'COMPLETED';
+          status = "COMPLETED";
         } else {
-          status = 'FULFILLED';
+          status = "FULFILLED";
         }
       } else {
-        status = 'UNFULFILLED';
+        status = "UNFULFILLED";
       }
       return (
         <ListItem
           styles={
-            filter === 'Unfulfilled' ? 'unfulfilledOnly' : 'upcomingPastDates'
+            filter === "Unfulfilled" ? "unfulfilledOnly" : "upcomingPastDates"
           }
           key={r.id}
           id={r.id}
@@ -92,29 +119,54 @@ const AdminHome = (props) => {
     return (
       <Filter
         styles={classes.filter}
-        title='Filter: '
+        title="Filter: "
         value={filter}
         onChange={handleChange}
       >
-        <option value='Unfulfilled'>Unfulfilled</option>
-        <option value='New Reviews'>New Reviews</option>
-        <option value='Completed'>Completed</option>
-        <option value='Cancelled'>Cancelled</option>
-        <option value='All'>All</option>
+        <option value="Unfulfilled">Unfulfilled</option>
+        <option value="New Reviews">New Reviews</option>
+        <option value="Completed">Completed</option>
+        <option value="All">All</option>
       </Filter>
     );
   };
 
   return (
-    <ListContainer
-      title={
-        filter === 'New Reviews' ? 'Newly Reviewed' : filter + ' ' + 'Requests'
-      }
-      styles={classes.requests}
-    >
-      {renderFilter()}
-      {renderRequests()}
-    </ListContainer>
+    <div className={classes.mainContainer}>
+      <div className={classes.buttonsDiv}>
+        <Button
+          onClick={() => {
+            setCancelledRequests({ ...cancelledRequests, show: false });
+            setNewlyReviewed({ ...newlyReviewed, show: false });
+          }}
+        >
+          Unfulfilled
+        </Button>
+        <Button
+          onClick={() => {
+            setCancelledRequests({ ...cancelledRequests, show: true });
+            setNewlyReviewed({ ...newlyReviewed, show: false });
+          }}
+        >
+          Cancelled{" "}
+          <span className={classes.italicFont}>
+            ({cancelledRequests.length ? cancelledRequests.length : 0})
+          </span>
+        </Button>
+        <Button
+          onClick={() => {
+            setNewlyReviewed({ ...newlyReviewed, show: true });
+            setCancelledRequests({ ...cancelledRequests, show: false });
+          }}
+        >
+          Newly Reviewed{" "}
+          <span className={classes.italicFont}>
+            ({newlyReviewed.length ? newlyReviewed.length : 0})
+          </span>
+        </Button>
+      </div>
+      <ListContainer>{renderRequests()}</ListContainer>
+    </div>
   );
 };
 
